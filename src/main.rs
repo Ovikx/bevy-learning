c57ffc7f7d0ef2211940eff0dfabb6e1e26a2537
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-11-06 23:55:29 -0600

    more setup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("John".to_string()));
+    commands.spawn().insert(Person).insert(Name("Jane".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello, {}!", name.0);
+    }
+}
+
 fn main() {
-    App::new().run();
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(add_people)
+    .add_system(hello_world)
+    .add_system(greet_people)
+    .run();
 }
